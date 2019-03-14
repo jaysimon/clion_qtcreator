f41b69f4992a29173025a89d1c6e5c7d8f86b3e5
simon <monkey1510@gmail.com>
2019-03-14 12:03:30 +0800

    add mainwindow.cpp correct

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,12 +1,33 @@
 #include "mainwindow.h"
-#include <QGridLayout>
-#include <QRadioButton>
-#include <QPushButton>
-#include <QCheckBox>
-#include <QMenu>
+#include "ui_mainwindow.h"
+#include "deal.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
+  ui->setupUi(this);
+  connect(ui->pbOriginSound, SIGNAL(clicked()), this, SLOT(change2_origin_sound()));
+  connect(ui->pbVecSound, SIGNAL(clicked()), this, SLOT(change2_vec_sound()));
+  connect(ui->pbVecNrSound, SIGNAL(clicked()), this, SLOT(change2_vec_nr_sound()));
+}
 
+void MainWindow::change2_origin_sound(){
+  iFlag = 0;
+  printf("%d", iFlag);
+}
+
+void MainWindow::change2_vec_sound(){
+  iFlag = 1;
+  printf("%d", iFlag);
+}
+
+void MainWindow::change2_vec_nr_sound(){
+  iFlag = 2;
+  printf("%d", iFlag);
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
 }
